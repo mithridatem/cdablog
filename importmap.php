9650cf7c5dcdf99b399259ea3611714b36a1ddb5
mithridatem <mathieumithridate@adrar-formation.com>
2024-03-04 09:43:42 +0100

    Correction exercice 10

--- importmap.php
+++ importmap.php
@@ -24,7 +24,7 @@ return [
     '@symfony/stimulus-bundle' => [
         'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
     ],
-    '@hotwired/turbo' => [
+    /* '@hotwired/turbo' => [
         'version' => '7.3.0',
-    ],
+    ], */
 ];
