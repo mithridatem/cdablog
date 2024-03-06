7d1880f055dc2d1b97b6c5bfdec72bdec620cecd
mithridatem <mathieumithridate@adrar-formation.com>
2024-03-06 15:04:01 +0100

    Exemple formulaire update

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -78,4 +78,45 @@ class ArticleController extends AbstractController
             'message' => $msg,
         ]);
     }
+
+    #[Route('/article/update/{id}', name:'app_update_article')]
+    public function updateArticle(Request $request, $id) : Response 
+    {
+
+        $msg = ""    ;
+        $article = $this->articleRepository->find($id);
+        //test si l'article n'existe pas
+        if(!$article) {
+            //redirection vers la page d'accueil
+            return $this->redirectToRoute('app_article_all');
+        }
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+        //tester si il est submit
+        if($form->isSubmitted() and $form->isValid()) {
+            /* dd($form->getData()->getDateCreation()->format('d-m-Y')); */
+            //nettoyer les entrées
+            $article->setTitre(UtilsService::cleanInput($article->getTitre()));
+            $article->setContenu(UtilsService::cleanInput($article->getContenu()));
+            $article->setUrlImg(UtilsService::cleanInput($article->getUrlImg()));
+            $article->setDateCreation( new \DateTimeImmutable(UtilsService::cleanInput
+            ($form->getData()->getDateCreation()->format('d-m-Y'))));
+            //récupération de l'article
+            $recup = $this->articleRepository->findOneBy(["titre"=>$article->getTitre(), 
+            "contenu"=> $article->getContenu()]);
+            //test si l'article existe 
+            if(!$recup) {
+                $this->em->persist($article);
+                $this->em->flush();
+                $msg = "L'article : " . $article->getTitre() . " a été modifié en BDD";
+            }
+            else{
+                $msg = "L'article existe déja en BDD";
+            }
+        }
+        return $this->render('article/update.html.twig', [
+            'form' => $form->createView(),
+            'message' => $msg,
+        ]);
+    }
 }
