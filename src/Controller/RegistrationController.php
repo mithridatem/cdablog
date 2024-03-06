7c63e45eba00f4b7e0193f2a2bacaeeef6a0aa13
mithridatem <mathieumithridate@adrar-formation.com>
2024-03-06 16:28:39 +0100

    feat : connexion

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -35,7 +35,7 @@ class RegistrationController extends AbstractController
 
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_article_all');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
