fc2395460f1d729d1eca23a0c5e4d55c7de7bc09
mithridatem <mathieumithridate@adrar-formation.com>
2024-03-07 16:08:36 +0100

    feat: template email

--- src/Controller/EmailController.php
+++ src/Controller/EmailController.php
@@ -5,17 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+use App\Service\EmailService;
 class EmailController extends AbstractController
 {
     #[Route('/email', name: 'app_email')]
-    public function index(): Response
+    public function index(EmailService $emailService): Response
     {
         $subject = "Veuillez activer votre compte";
         $body = "Veuillez trouver ci-dessous le lien pour activer votre compte";
-        return $this->render('email/index.html.twig', [
+        $content = $this->render('email/index.html.twig', [
             'subject' => $subject,
             'body' => $body,
         ]);
+        /* dd($content->getContent()); */
+        $emailService->sendEmail('mathieumithridate@adrar-formation.com',$subject, $content->getContent());
+        return new Response('Le mail à bien été envoyé');
     }
 }
